c7d2ec3e4315f285c2c75fb673c772b4182e419d
chippytech <80016011+chippytech@users.noreply.github.com>
2023-09-16 08:30:12 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <?php
-echo "<h1> Welcome to GeneratePanel </h1>";
-echo "<a href='myhostingaccounts.php'> My Hosting Accounts </a>";
+echo "<h1>Welcome to GeneratePanel</h1>";
+echo "<a href='myhostingaccounts.php'>My Hosting Accounts</a>";
+
+// Additional content
+echo "<p>Welcome to GeneratePanel, your one-stop hosting control panel!</p>";
+echo "<h2>Features:</h2>";
+echo "<ul>";
+echo "<li>Easy website management</li>";
+echo "<li>Secure hosting solutions</li>";
+echo "<li>24/7 customer support</li>";
+echo "</ul>";
 ?>
